7de1866ed2b526245ab1c559e77de9f1764200b6
Schildkrote-Communist <108930911+Schildkrote-Communist@users.noreply.github.com>
2023-01-29 19:02:33 +0100

    Add files via upload

---
+++ router/authRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = require("../../nodeProject/serverCode/routes/blogRoute");
+const Router = express.Router();
+const controller = require("../controllers/authController");
+
+
+router.get("/signup", controller.signup_get);
+
+router.post("/signup", controller.signup_post);
+
+router.get("/login", controller.login_get);
+
+router.post("/login", controller.login_post);
+
+router.get("/logout", controller.logout);
+
+router.get("/user/:username", controller.userInfo);
+module.exports = router;
